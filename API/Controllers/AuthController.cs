5a533da2c424bfbc3c11ffd97b314f1c1e0cfb15
Olexander <zagariya@knu.ua>
2018-04-07 20:54:03 +0000

    First commit

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+using API.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private IConfiguration Configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult RequestToken([FromBody] TokenRequestModel request)
+        {
+
+            
+            if (request.Username == "login" && request.Password == "password")
+            {
+                var claims = new[]
+                {
+            new Claim(ClaimTypes.Name, request.Username)
+        };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SecurityKey"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "localhost",
+                    audience: "localhost",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(10),
+                    signingCredentials: creds);
+                    
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return BadRequest("Could not verify username and password");
+        }
+    }
+}
